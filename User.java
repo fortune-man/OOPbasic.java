ea16580b924a66a0804920cb9ae770113e38f622
yourtimeislimited <79819812+yourtimeislimited@users.noreply.github.com>
2022-01-20 20:16:38 +0900

    Add files via upload

---
+++ User.java
@@ -0,0 +1,10 @@
+public class User {
+    String id;
+    String password;
+
+    String login(String id, String password) {
+        this.id = id;
+        this.password = password;
+        return  "id: " + id + "| password: " + password;
+    }
+}
